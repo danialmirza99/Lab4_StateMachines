c7ad7413acb2afd6e678233b228a76aba666cdfe
danialmirza99 <61335608+danialmirza99@users.noreply.github.com>
2021-04-18 22:46:53 -0700

    Update dmirz001_lab4_part2_tests.py

--- turnin/dmirz001_lab4_part2_tests.py
+++ turnin/dmirz001_lab4_part2_tests.py
@@ -1,127 +1,46 @@
-/*	Author: dmirz001
- *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab 4  Exercise 2
- *	Exercise Description: [optional - include for your own benefit]
- *
- *	I acknowledge all content contained herein, excluding template or example
- *	code, is my own original work.
- */
-#include <avr/io.h>
-#ifdef _SIMULATE_
-#include "simAVRHeader.h"
-#endif
+# Array of tests to run (in order)
+# Each test contains
+#   description - 
+#   steps - A list of steps to perform, each step can have
+#       inputs - A list of tuples for the inputs to apply at that step
+#       *time - The time (in ms) to wait before continuing to the next step 
+#           and before checking expected values for this step. The time should be a multiple of
+#           the period of the system
+#       *iterations - The number of clock ticks to wait (periods)
+#       expected - The expected value at the end of this step (after the "time" has elapsed.) 
+#           If this value is incorrect the test will fail early before completing.
+#       * only one of these should be used
+#   expected - The expected output (as a list of tuples) at the end of this test
+# An example set of tests is shown below. It is important to note that these tests are not "unit tests" in 
+# that they are not ran in isolation but in the order shown and the state of the device is not reset or 
+# altered in between executions (unless preconditions are used).
 
-enum States { init, wait, inc_wait, inc, dec_wait, dec, reset } state;
+tests = [ {'description': 'PINA: 0x01 => PORTC: 0x08',
+    'steps': [{'inputs': [('PINA', 0x01)], 'iterations': 2},],
+    'expected': [('PORTC',0x08)],
+    },
+        {'description': 'PINA: 0x02 => PORTB: 0x06',
+    'steps': [{'inputs': [('PINA', 0x02)], 'iterations': 2},],
+    'expected': [('PORTC',0x06)],
+    },
+        {'description': 'PINA: 0x02, 0x02, 0x02, 0x01 => PORTC: 0x01',
+    'steps': [{'inputs': [('PINA', 0x02)], 'iterations': 1},
+        {'inputs': [('PINA', 0x02)], 'iterations': 1},
+        {'inputs': [('PINA', 0x02)], 'iterations': 1},
+        {'inputs': [('PINA', 0x01)], 'iterations': 1}],
+    'expected': [('PORTC',0x04)],
+    },
+        {'description': 'PINA: 0x03, 0x01, 0x01 => PORTB: 0x02',
+    'steps': [{'inputs': [('PINA', 0x03)], 'iterations': 2},
+        {'inputs': [('PINA', 0x01)], 'iterations': 100},
+        {'inputs': [('PINA', 0x01)], 'iterations': 2},
+        {'inputs': [('PINA', 0x01)], 'iterations': 2}],
+    'expected': [('PORTC',0x03)],
+    },
 
-void TickFct(){
-	switch(state){
-		case init:
-		state = wait;
-		break;
+    ]
+watch = ['state']
 
-		case wait:
-		if((PINA & 0x01) == 0x01){
-			state = inc_wait;
-		}
-		else if ((PINA & 0x02) == 0x02){
-			state = dec_wait;
-		}
-		else if ((PINA & 0x03) == 0x03){
-			state = reset;
-		}
-		else{
-			state = wait;
-		}
-		break;
-
-		case inc_wait:
-                state = inc;
-                break;
-
-		case dec_wait:
-                state = dec;
-                break;
-
-		case inc:
-                if((PINA & 0x03) == 0x03){
-                        state = reset;
-                }
-                else{
-                	state = wait;
-                }
-                break;
-
-		case dec:
-                if((PINA & 0x03) == 0x03){
-                        state = reset;
-                }
-                else{
-                        state = wait;
-                }
-                break;
-
-		case reset:
-                if((PINA & 0x03) == 0x03){
-                        state = reset;
-                }
-		else{
-			state = wait;
-		}
-                break;
-
-		default:
-		state = init;
-	}
-
-	switch(state){
-		case init:
-		PORTC = 0x07;
-		break;
-
-		case wait:
-		break;
-
-		case inc_wait:
-		break;
-
-		case dec_wait:
-		break;
-
-		case inc:
-		if (PORTC < 0x09){
-			PORTC= PORTC + 1;
-		}
-		else{
-			PORTC = PORTC;
-		}
-		break;
-
-		case dec:
-		if(PORTC > 0x00){
-			PORTC= PORTC - 1;
-		}
-		else{
-			PORTC = PORTC;
-		}
-		break;
-
-		case reset:
-		PORTC = 0x00;
-		
-		default:
-		PORTC = 0x07;
-		break;
-	}
-
-}
-
-
-int main(void) {
-	DDRA = 0x00; PORTA = 0xFF;
-	DDRC = 0xFF; PORTB = 0x00;
-	while (1){
-		TickFct();
-	}
-	return 1;
-}
+# Optionally you can add a set of "watch" variables these need to be global or static and may need
+# to be scoped at the function level (for static variables) if there are naming conflicts. The 
+# variables listed here will display everytime you hit (and stop at) a breakpoint
