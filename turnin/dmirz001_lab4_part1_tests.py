090f76dadfadcc6a931ddcf9efa6fe7bded1482d
Danial <danialmirza99>
2021-04-18 20:55:08 -0700

    Part 1 Revise

--- turnin/dmirz001_lab4_part1_tests.py
+++ turnin/dmirz001_lab4_part1_tests.py
@@ -15,14 +15,39 @@
 # that they are not ran in isolation but in the order shown and the state of the device is not reset or 
 # altered in between executions (unless preconditions are used).
 
-tests = [ {'description': 'PINA: 0x00 => PORTB: 1, state: On_0',
-    'steps': [{'inputs': [('PINA', 0x00)], 'iterations': 2},
+tests = [ {'description': 'PINA: 0x00, 0x00, 0x00 => PORTB: 0x01',
+    'steps': [{'inputs': [('PINA', 0x01)], 'iterations': 2},
+        {'inputs': [('PINA', 0x00)], 'iterations': 100},
         {'inputs': [('PINA', 0x00)], 'iterations': 2},
         {'inputs': [('PINA', 0x00)], 'iterations': 2}],
     'expected': [('PORTB',0x01)],
     },
+	{'description': 'PINA: 0x01, 0x01, 0x01 => PORTB: 0x02',
+    'steps': [{'inputs': [('PINA', 0x01)], 'iterations': 2},
+        {'inputs': [('PINA', 0x01)], 'iterations': 100},
+        {'inputs': [('PINA', 0x01)], 'iterations': 2},
+        {'inputs': [('PINA', 0x01)], 'iterations': 2}],
+    'expected': [('PORTB',0x02)],
+    },
+	{'description': 'PINA: 0x00, 0x01, 0x01 => PORTB: 0x02',
+    'steps': [{'inputs': [('PINA', 0x01)], 'iterations': 2},
+        {'inputs': [('PINA', 0x00)], 'iterations': 100},
+        {'inputs': [('PINA', 0x01)], 'iterations': 2},
+        {'inputs': [('PINA', 0x01)], 'iterations': 2}],
+    'expected': [('PORTB',0x02)],
+    },
+	{'description': 'PINA: 0x01, 0x01, 0x00 => PORTB: 0x01',
+    'steps': [{'inputs': [('PINA', 0x01)], 'iterations': 2},
+        {'inputs': [('PINA', 0x01)], 'iterations': 100},
+        {'inputs': [('PINA', 0x01)], 'iterations': 2},
+        {'inputs': [('PINA', 0x00)], 'iterations': 2}],
+    'expected': [('PORTB',0x01)],
+    },
+
     ]
-watch = ['state']
+
+
+
 
 
 
